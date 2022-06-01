807f701977997a58fba0cc75722f71bd1de11cd7
hugo meiland <hugo.meiland@microsoft.com>
2022-06-01 15:35:10 +0200

    start first eb build

--- easybuild/install_apps.sh
+++ easybuild/install_apps.sh
@@ -5,8 +5,8 @@ export EASYBUILD_PREFIX=/apps/easybuild/el7/avx_512
 export EASYBUILD_REPOSITORYPATH=~/.local/easybuild/easyconfigs
 
 mkdir -p /mnt/resource${EASYBUILD_PREFIX}
-sudo mkdir /apps
-sudo chmod 777 /apps
+sudo mkdir -p /apps/easybuild/el7
+sudo chmod 777 /apps/easybuild/el7
 ln -s /mnt/resource${EASYBUILD_PREFIX} ${EASYBUILD_PREFIX}
 
 eb ${EASYBUILD_REPOSITORYPATH}/m/M4/M4-1.4.19.eb
