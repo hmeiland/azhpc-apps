0d577c11a699a4c762a0ad036fe9422025f877bb
hugo meiland <hugo.meiland@microsoft.com>
2022-06-01 17:40:05 +0200

    start first eb build

--- easybuild/install_apps.sh
+++ easybuild/install_apps.sh
@@ -5,10 +5,12 @@ export EASYBUILD_PREFIX=/apps/easybuild/el7/avx_512
 export EASYBUILD_PACKAGEPATH=/easybuildrepo
 export EASYBUILD_REPOSITORYPATH=~/.local/easybuild/easyconfigs
 
-mkdir -p /mnt/resource${EASYBUILD_PREFIX}
-sudo mkdir -p /apps/easybuild/el7
-sudo chmod 777 /apps/easybuild/el7
-ln -s /mnt/resource${EASYBUILD_PREFIX} ${EASYBUILD_PREFIX}
+if [[ ! -d /apps ]]; then
+  mkdir -p /mnt/resource${EASYBUILD_PREFIX}
+  sudo mkdir -p /apps/easybuild/el7
+  sudo chmod 777 /apps/easybuild/el7
+  ln -s /mnt/resource${EASYBUILD_PREFIX} ${EASYBUILD_PREFIX}
+fi
 
 eb ${EASYBUILD_REPOSITORYPATH}/f/FPM/FPM-1.3.3-Ruby-2.1.6.eb --robot
 module use ${EASYBUILD_PREFIX}/modules/all
