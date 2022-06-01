18d9935e00541f36581e8d56cc0a39b5be405238
hugo meiland <hugo.meiland@microsoft.com>
2022-06-01 16:41:24 +0200

    start first eb build

--- easybuild/install_apps.sh
+++ easybuild/install_apps.sh
@@ -9,4 +9,9 @@ sudo mkdir -p /apps/easybuild/el7
 sudo chmod 777 /apps/easybuild/el7
 ln -s /mnt/resource${EASYBUILD_PREFIX} ${EASYBUILD_PREFIX}
 
+eb ${EASYBUILD_REPOSITORYPATH}/f/FPM/FPM-1.3.3-Ruby-2.1.6.eb --robot
+module use ${EASYBUILD_PREFIX}/modules/all
+ml load FPM/1.3.3-Ruby-2.1.6
+
 eb ${EASYBUILD_REPOSITORYPATH}/m/M4/M4-1.4.19.eb
+eb --package ${EASYBUILD_REPOSITORYPATH}/m/M4/M4-1.4.19.eb --skip --rebuild
