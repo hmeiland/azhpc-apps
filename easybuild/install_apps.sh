c21063511b55cc2fe7875a2093726cc9f4a8cdc6
hugo meiland <hugo.meiland@microsoft.com>
2022-06-01 17:15:38 +0200

    start first eb build

--- easybuild/install_apps.sh
+++ easybuild/install_apps.sh
@@ -2,6 +2,7 @@
 
 
 export EASYBUILD_PREFIX=/apps/easybuild/el7/avx_512
+export EASYBUILD_PACKAGEPATH=/easybuildrepo
 export EASYBUILD_REPOSITORYPATH=~/.local/easybuild/easyconfigs
 
 mkdir -p /mnt/resource${EASYBUILD_PREFIX}
@@ -15,3 +16,6 @@ ml load FPM/1.3.3-Ruby-2.1.6
 
 eb ${EASYBUILD_REPOSITORYPATH}/m/M4/M4-1.4.19.eb
 eb --package ${EASYBUILD_REPOSITORYPATH}/m/M4/M4-1.4.19.eb --skip --rebuild
+
+
+createrepo ${EASYBUILD_PACKAGEPATH}
