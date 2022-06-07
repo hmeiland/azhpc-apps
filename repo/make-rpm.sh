86a1db7aeb25ad2fc2a489bcee7c5e50abec22ad
hugo meiland <hugo.meiland@microsoft.com>
2022-06-07 15:57:39 +0200

    write file from spec

--- repo/make-rpm.sh
+++ repo/make-rpm.sh
@@ -2,6 +2,7 @@
 
 mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
 git checkout azurehpc-application.repo
-tar -cf azurehpc-application-repo.tar azurehpc-application.repo
-cp azurehpc-application-repo.tar ~/rpmbuild/SOURCES
+#tar -cf azurehpc-application-repo.tar azurehpc-application.repo
+#cp azurehpc-application-repo.tar ~/rpmbuild/SOURCES
 rpmbuild -ba azurehpc-application-repository.spec
+cp ~/rpmbuild/RPMS/noarch/azurehpc-application-repository-*-1.el7.noarch.rpm .
